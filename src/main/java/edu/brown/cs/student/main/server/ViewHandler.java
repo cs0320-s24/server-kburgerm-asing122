36c2e4cd77e587e0253c6f443660dbf416d4e94f
asing122 <annika_singh@brown.edu>
2024-02-16 10:51:55 -0500

    docs: added documentation for viewhandler

--- src/main/java/edu/brown/cs/student/main/server/ViewHandler.java
+++ src/main/java/edu/brown/cs/student/main/server/ViewHandler.java
@@ -7,14 +7,30 @@ import spark.Request;
 import spark.Response;
 import spark.Route;
 
+/**
+ * A handler for viewing loaded CSV data.
+ */
 public class ViewHandler implements Route {
 
   List<List<String>> loadedFile;
 
+  /**
+   * Constructs a ViewHandler object with the given loaded CSV data.
+   *
+   * @param loadedFile The loaded CSV data.
+   */
   public ViewHandler(List<List<String>> loadedFile) {
     this.loadedFile = loadedFile;
   }
 
+  /**
+   * Handles HTTP requests for viewing loaded CSV data.
+   *
+   * @param request The HTTP request object.
+   * @param response The HTTP response object.
+   * @return A map containing the loaded CSV data.
+   * @throws Exception If an error occurs during request handling.
+   */
   @Override
   public Object handle(Request request, Response response) throws Exception {
     Map<String, Object> responseMap = new HashMap<>();
