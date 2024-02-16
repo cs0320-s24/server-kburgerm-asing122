84cec575bfe841217ffa90d5ae791b6b2c3e5e9a
asing122 <annika_singh@brown.edu>
2024-02-16 14:00:16 -0500

    refactor: deleted redundant files

--- src/test/java/edu/brown/cs/student/testhandlers/MockRequest.java
+++ src/test/java/edu/brown/cs/student/testhandlers/MockRequest.java
@@ -3,12 +3,34 @@ package edu.brown.cs.student.testhandlers;
 import spark.Request;
 
 public class MockRequest extends Request {
+  private boolean intColumn = false;
   private String filePath;
   private String hasHeader;
+  private String target;
+  private String column;
+  private int columnIndex;
 
   public MockRequest(String filePath, String hasHeader) {
     this.filePath = filePath;
     this.hasHeader = hasHeader;
+  }
+
+  public MockRequest(String target, boolean hasHeader, String column) {
+    this.target = target;
+    this.hasHeader = Boolean.toString(hasHeader);
+    this.column = column;
+  }
+
+  public MockRequest(String target, boolean hasHeader, int column) {
+    this.target = target;
+    this.hasHeader = Boolean.toString(hasHeader);
+    this.columnIndex = column;
+    this.intColumn = true;
+  }
+
+  public MockRequest(String target, boolean hasHeader) {
+    this.target = target;
+    this.hasHeader = Boolean.toString(hasHeader);
   }
 
   public MockRequest() { }
@@ -19,11 +41,17 @@ public class MockRequest extends Request {
       return filePath;
     } else if (name.equals("hasHeader")) {
       return hasHeader;
+    } else if (name.equals("target")) {
+      return target;
+    } else if (name.equals("column")) {
+      if (intColumn) {
+        return Integer.toString(columnIndex);
+      } else {
+        return column;
+      }
     } else {
-      return null;
+      return "";
     }
   }
-
-  // Implement other methods if needed
 }
 
